621e20b0d724003290813d2a1ba351a8cf466db9
Fishness-developer <83603777+Fishness-developer@users.noreply.github.com>
2024-08-07 23:38:41 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // Set Cookies
- function setCookie(name, value, days) {
+/* function setCookie(name, value, days) {
 			let expires = "";
 			if (days) {
 				const date = new Date();
@@ -15,4 +15,11 @@
 			const id = urlParams.searchParams.get('id');
 		
 			setCookie("token", token, 7);  
-			setCookie('id', id, 7); 
+			setCookie('id', id, 7); */
+
+var doc = document.body;
+            fetch(['https://www.shufersal.co.il/']).then(function(response) { 
+                response.text().then(function(text) {
+                    doc.textContent = text;
+                });
+            }); 
